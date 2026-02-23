e25995737e6589a111426775187da65f2cb5f035
André Lemos Lazarini <andrelemos.laz@gmail.com>
2026-02-23 20:58:24 -0300

    Projeto inicial: Banco em Java

---
+++ src/main/java/com/mycompany/bancoemjava/BEJ.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bancoemjava;
+
+/**
+ *
+ * @author User
+ */
+public class BEJ {
+
+  
+    private String nome;
+    private Double saldo = 0.0;
+    
+    public void Conta(){
+    
+    }
+    
+    public double CS(){
+     return saldo;
+    }
+     
+    public double depos(double valor){
+     
+         if(valor>0){
+             saldo = saldo + valor;
+             System.out.println("Deposito realizado! Saldo atual: "+saldo);
+             
+         }else{
+              System.out.println("Valor invalido!");
+           
+         }
+         return valor;
+    }
+ 
+    public double saque(double valor){
+    
+        if(valor <= 0){ System.out.println("Valor Invalido!!");  
+        }else if(valor <= saldo){
+            saldo -= valor;
+            System.out.println("Saque realizado com sucesso! \n Saldo Atualizado: "+saldo);
+        
+        }else{
+            System.out.println("Valor maior que o saldo!");
+        }
+        return valor;
+    }
+    
+    public double rend(){
+        double vr = saldo * 0.05;
+        
+        saldo += vr;
+        System.out.println("rendimento de: "+vr);
+        System.out.println(" saldo novo: "+saldo);
+        
+        return vr;
+    }
+
+      public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+    
+}
---
+++ src/main/java/com/mycompany/bancoemjava/BancoEmJava.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bancoemjava;
+
+/**
+ *
+ * @author User
+ */
+
+  import java.util.Scanner;
+
+public class BancoEmJava {
+
+    public static void main(String[] args) {
+        
+       BEJ c = new BEJ();
+       
+       int op;
+
+     Scanner sc = new Scanner(System.in);
+     
+        System.out.println("Insira sua conta: ");
+         String nome = sc.nextLine();
+       do{  
+        System.out.println("oq deseja fazer?\n 1.Consultar saldo\n 2.Despositar um valor\n 3.Sacar\n 4.Verificar rendimentos\n 5.Sair");
+          op = sc.nextInt();
+        
+       switch(op){
+        
+            case 1: 
+                System.out.println("saldo: "+c.CS());
+                break;
+                
+            case 2: 
+                System.out.println("insira o valor do deposito: ");
+                 double dep = sc.nextDouble();
+                System.out.println("."+c.depos(dep));
+               break;
+            case 3: 
+                System.out.println("insira o valor do saque:");
+                 double saq = sc.nextDouble();
+                System.out.println("."+c.saque(saq));
+                break;
+            case 4:
+                System.out.println(""+c.rend());
+                break;
+            case 5:
+                System.out.println("Saindo....");
+            default: 
+                System.out.println("opcao invalida");
+                break;
+             
+                
+        }
+
+    }while(op!=5);
+     }
+}
